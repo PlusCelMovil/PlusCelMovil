04bb1aa7f3e3ba2a0da672da699995bd6c96aa17
Pablo Pincheira <pincheirapabloj@gmail.com>
2015-11-11 14:34:03 -0300

    Agregue clases para realizar pruebas de lectura

--- app/src/main/java/com/pluscel/pluscelmovil/ServicesActivity.java
+++ app/src/main/java/com/pluscel/pluscelmovil/ServicesActivity.java
@@ -5,13 +5,39 @@ package com.pluscel.pluscelmovil;
  */
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View;
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
-import com.pluscel.pluscelmovil.serv.Services;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+import com.pluscel.pluscelmovil.dom.*;
+import com.pluscel.pluscelmovil.serv.*;
 public class ServicesActivity  extends Activity  {
 
     String url;
@@ -64,6 +90,73 @@ public class ServicesActivity  extends Activity  {
         intent.putExtra("pass", pass);
 
         startActivity(intent);
+    }
+
+    private class FillListOfServicesThread extends AsyncTask<Void, Void, Services> {
+        @Override
+        protected Services doInBackground(Void... params) {
+            try {
+
+
+                //Services services = null;
+                Log.v("ingresando User y Pass", user + " : " + pass);
+                // Set the username and password for creating a Basic Auth request
+                HttpAuthentication authHeader = new HttpBasicAuthentication(user, pass);
+                HttpHeaders requestHeaders = new HttpHeaders();
+                requestHeaders.setAuthorization(authHeader);
+                HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
+
+                Log.v("ingresando URL",url);
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+                // Make the HTTP GET request to the Basic Auth protected URL
+                ResponseEntity<RestLinks> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, RestLinks.class);
+
+                //return response.getBody();
+
+                RestLinks restLinks = response.getBody();
+
+                Log.v("leido", restLinks.getLinks().size()+"");
+
+                //Buscar los servicios
+
+                for (RestLink restlink : restLinks.getLinks()){
+
+                    if (restlink.getRel().equals("urn:org.restfulobjects:rels/services")){
+                        Log.v("Servicios Encontrados en", restlink.getHref());
+
+
+                        // Make the HTTP GET request to the Basic Auth protected URL
+                        ResponseEntity<Services> response2 = restTemplate.exchange(restlink.getHref(), HttpMethod.GET, requestEntity, Services.class);
+
+                        services = response2.getBody();
+
+                        Log.v("leido", services.getValue().size()+"");
+
+                        //Buscar los servicios
+
+                        int arraySize = services.getValue().size();
+                        IsisService[] serviceArray = new IsisService[arraySize];
+                        for (int i=0; i< arraySize;i++){
+                            serviceArray[i] = services.getValue().get(i);
+                            Log.v("Servicios Encontrados", serviceArray[i].getTitle());
+                        }
+
+                    }
+
+
+
+                }
+
+                return services;
+
+            } catch (Exception e) {
+                Log.e("main_activity", e.getMessage(), e);
+            }
+
+            return null;
+        }
     }
 
 }
